c44e8e2eeee323bd9042f0788a5e9e563f7de4e9
AbidiYassine-etu <98038872+AbidiYassine-etu@users.noreply.github.com>
2023-10-25 07:38:59 +0100

    Delete TD2 directory

--- TD2/src/main/java/repository/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Model.Employee;
-
-public interface EmployeeRepository  extends JpaRepository<Employee, Long>{
-
-}
